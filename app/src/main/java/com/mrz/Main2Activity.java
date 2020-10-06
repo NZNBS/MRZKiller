5f3b3bcb5baa199ba737f73f4e9a306e47e1ce5a
n63qwj6nqw3 <arv3av5av@wb6236wb.com>
2020-10-06 16:30:36 +0200

    Adding nagivation bar

--- app/src/main/java/com/mrz/Main2Activity.java
+++ app/src/main/java/com/mrz/Main2Activity.java
@@ -1,25 +1,22 @@
-package com.mrz.activity;
+package com.mrz;
 
 import android.os.Bundle;
-
-import android.app.Activity;
-import android.view.Menu;
 import android.view.View;
+import android.view.Menu;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.snackbar.Snackbar;
-import com.mrz.R;
-
-public class MRZActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
 
