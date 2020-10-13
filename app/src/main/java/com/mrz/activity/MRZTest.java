f85c83dc02a362777a1a004280bafb9054d2c966
MrEoZ <nazabusi@gmail.com>
2020-10-13 19:24:50 -0300

    Fixing bugs

--- app/src/main/java/com/mrz/activity/MRZTest.java
+++ app/src/main/java/com/mrz/activity/MRZTest.java
@@ -1,14 +1,18 @@
 package com.mrz.activity;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.mrz.R;
 import com.mrz.stuff.FileUtils;
@@ -46,6 +50,15 @@ public class MRZTest extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        ActivityCompat.requestPermissions(MRZTest.this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                1);
+        ActivityCompat.requestPermissions(MRZTest.this,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                2);
+        ActivityCompat.requestPermissions(MRZTest.this,
+                new String[]{Manifest.permission.READ_PHONE_STATE},
+                3);
         Intent intent = new Intent(this, MRZPlugins.class);
         findViewById(R.id.pluginlist).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,7 +74,52 @@ public class MRZTest extends AppCompatActivity {
             }
         });
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
 
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    finish();
+                    Toast.makeText(MRZTest.this, "Please allow :(", Toast.LENGTH_SHORT).show();
+                }
+            }
+            case 2: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    finish();
+                    Toast.makeText(MRZTest.this, "Please allow :(", Toast.LENGTH_SHORT).show();
+                }
+            }
+            case 3: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    finish();
+                    Toast.makeText(MRZTest.this, "Please allow :(", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
     private static final int REQUEST_CHOOSER = 1234;
 
     @Override
@@ -77,7 +135,7 @@ public class MRZTest extends AppCompatActivity {
                     String path = FileUtils.getPath(this, uri);
 
                     // Alternatively, use FileUtils.getFile(Context, Uri)
-                    if (path != null && FileUtils.isLocal(path) && path.equals(".mrz")) {
+                    if (FileUtils.isLocal(path) && path.equals(".mrz")) {
                         try {
                             @SuppressLint({"NewApi", "LocalSuppress"}) byte[] bytes = Files.readAllBytes(Paths.get(path));
                             xPluginManager.Setup(this, bytes);
